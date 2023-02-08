5bea232946ddaf526716dea2462168246257ef44
RITE  [ ПРОГРАМУВАННЯ ] <116145988+RITEUA@users.noreply.github.com>
2023-02-08 11:49:20 +0200

    Add files via upload
    Додано файл main.py, який запускає telegram bot, котрий відповідає на команди /start і /get.
    При надсиланні команди /start, бот надсилає повідомлення: "Надішли команду /get, для перегляду столиць країн",
    а при надсиланні команди /get, список із трьох країн, та їх столиці (України, Англії та Польщі)

---
+++ main.py
@@ -0,0 +1,25 @@
+from aiogram import Bot, Dispatcher, executor, types
+import logging
+from select_data_from_mysql_table import select_data
+
+TOKEN = "ТОКЕН ВАШОГО БОТА"  # СКОПІЮЙТЕ ЙОГО У @BotFather
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands="start")
+async def starting(message: types.Message):
+    # Перше, що буде надіслано користувачу, після натиску кнопки start, або команди /start
+    await message.answer("Надішли команду /get, для перегляду столиць країн")
+
+
+@dp.message_handler(commands="get")
+async def starting(message: types.Message):
+    """ Надсилає повідомлення, у якому містяться країни: столиці,
+        отримані з файлу select_data_from_mysql_table.py """
+    await message.answer(select_data())
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
